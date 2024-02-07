7e98e6d8d94dbc97e5ddbf440afeb13599397808
Yaoyx <yxiaoy@umich.edu>
2024-02-07 14:51:08 -0800

    support both fastq and fastq.gz

--- frip_sm/calculate_frip.py
+++ frip_sm/calculate_frip.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from optparse import OptionParser
 import pysam
 import deeptools.countReadsPerBin as crpb
@@ -5,12 +6,17 @@ import deeptools.countReadsPerBin as crpb
 def main():
     usage = 'usage: %prog [options] <bam_file1> <bed_file>'
     parser = OptionParser(usage)
+
     parser.add_option('--nproc', dest='nproc',
         default=1,
         help='How many processes to use for calculation. Default: nproc=1')
     parser.add_option('--output-prefix', dest='prefix',
         default='',
         help='What is the prefix of output file')
+    parser.add_option('--output-directory', dest='outdir',
+        default='./',
+        help='What is the output directory for output file')
+    
     (options, args) = parser.parse_args()
 
     if len(args) != 2:
@@ -18,20 +24,20 @@ def main():
     else:
         bam = args[0]
         bed = args[1]
-        
+
     bam_files = [bam]
-    reads_counter = crpb.CountReadsPerBin(bam_files, bedFile=bed, numberOfProcessors=options.nproc)
+    reads_counter = crpb.CountReadsPerBin(bam_files, bedFile=bed, numberOfProcessors=int(options.nproc))
     reads_at_peaks = reads_counter.run()
     total_reads_at_peaks = reads_at_peaks.sum(axis=0)
 
     alignment = pysam.AlignmentFile(bam)
     frip = float(total_reads_at_peaks[0])/alignment.mapped
 
-    file_name = options.prefix + "_frip.txt"
-
-    file = open(file_name,"a")
+    filename = options.outdir + options.prefix + "_frip.txt"
+    file = open(filename,"a")
     file.write(options.prefix + "_frip\n")
     file.write(str(frip)+ "\n")
     file.close()
+    
 if __name__ == '__main__':
     main()
